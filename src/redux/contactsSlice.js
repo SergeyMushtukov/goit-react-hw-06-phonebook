f6b17a058ac222755b13c74e56f3045492baa2f5
SergeyMushtukov <hrystik90@gmail.com>
2023-08-28 21:50:28 +0300

    Отредактировал структура стейта для контактов

--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -3,12 +3,14 @@ import { nanoid } from 'nanoid';
 import { persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 
-const contactsInitialState = [
-  { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-  { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-  { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-  { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-];
+const contactsInitialState = {
+  contacts: [
+    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+  ],
+};
 
 const contactsSlice = createSlice({
   name: 'contacts',
@@ -16,7 +18,7 @@ const contactsSlice = createSlice({
   reducers: {
     addContact: {
       reducer(state, action) {
-        return [action.payload, ...state];
+        return {contacts:[action.payload, ...state.contacts]};
       },
       prepare(data) {
         return {
@@ -28,7 +30,7 @@ const contactsSlice = createSlice({
       },
     },
     deleteContact(state, action) {
-      return state.filter(contact => contact.id !== action.payload);
+      return {contacts: state.contacts.filter(contact => contact.id !== action.payload)};
     },
   },
 });
